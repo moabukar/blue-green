63b7b2aed038cdb30df86978e249e44b845de502
moabukar <mo-abukar10@hotmail.com>
2023-11-14 14:49:32 +0000

    add codedeploy resources

--- ecs/variables.tf
+++ ecs/variables.tf
@@ -8,3 +8,16 @@ variable "lb_target_group_name" {
   type    = string
   default = "tg"
 }
+
+variable "region" {
+  type = string
+  default = "us-east-1"
+}
+
+variable "aws_account_id" {
+  type = string
+}
+
+variable "aws_account_region" {
+  type = string
+}
